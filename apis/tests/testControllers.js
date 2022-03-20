60bd7a1047292aa7d27e9ee23cd6e6befb5f65af
Ahmad <ahmad_baq@hotmail.com>
2022-03-20 21:45:33 +0300

    add test update and test delete

--- apis/tests/testControllers.js
+++ apis/tests/testControllers.js
@@ -1,10 +1,25 @@
 const Test = require("../../models/Test");
 
+exports.controllerFetchTest = async (usertestId, next) => {
+  try {
+    const userTest = await Test.findById(usertestId);
+
+    if (userTest) return userTest;
+    else {
+      const error = new Error("userTest ID was not found!");
+      error.status = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.controllerGetTests = async (req, res, next) => {
   try {
-    const tests = await Test.find();
+    const userTests = await Test.find();
 
-    res.json({ msg: "Tests fetched", payload: tests });
+    res.json({ msg: "Tests fetched", payload: userTests });
   } catch (error) {
     next(error);
   }
@@ -15,6 +30,33 @@ exports.controllerAddTest = async (req, res, next) => {
     const testUser = req.body;
     const createdTestUser = await Test.create(testUser);
     res.status(200).json({ msg: "Test Created", payload: createdTestUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.controllerUpdateTest = async (req, res, next) => {
+  try {
+    const id = req.usertest._id;
+    const userTest = req.body;
+    console.log("hello");
+    const updatedTest = await Test.findByIdAndUpdate(id, userTest, {
+      runValidators: true,
+      new: true,
+    });
+    res.status(200).json({
+      msg: "Test Updated",
+      payload: updatedTest,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.controllerDeleteTest = async (req, res, next) => {
+  try {
+    await req.usertest.remove();
+    res.status(204).end();
   } catch (error) {
     next(error);
   }
--- apis/tests/testRoutes.js
+++ apis/tests/testRoutes.js
@@ -1,10 +1,37 @@
 const express = require("express");
-const { controllerGetTests, controllerAddTest } = require("./testControllers");
+const {
+  controllerGetTests,
+  controllerAddTest,
+  controllerUpdateTest,
+  controllerFetchTest,
+  controllerDeleteTest,
+} = require("./testControllers");
 
 const router = express.Router();
+
+router.param("usertestId", async (req, res, next, usertestId) => {
+  console.log("usertestId", usertestId);
+  const userTest = await controllerFetchTest(usertestId, next);
+  if (userTest) {
+    req.usertest = userTest;
+    next();
+  } else {
+    const err = new Error("Trip Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
 
 router.get("/", controllerGetTests);
 
 router.post("/", controllerAddTest);
+
+router.put(
+  "/:usertestId",
+
+  controllerUpdateTest
+);
+
+router.delete("/:usertestId", controllerDeleteTest);
 
 module.exports = router;
